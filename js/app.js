bee7ee8ec071fa0dac51ce5b8e27396c1eba23d3
Ben S <allobon@gmail.com>
2015-09-02 17:16:59 +0100

    pub

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
+
+	// show slider value in html view
 	$("#range-slider").on("input", function(event){
 		var rangeValue = $("#range-slider").val();
 		$("#how-much-value").html(rangeValue)
 	});
+
+	// parse slider value and pass to code generator
+	$("#codeGenerate").click(function(){
+		var rangeValue = $("#range-slider").val();
+		console.log(rangeValue);
+	})
+
 });
